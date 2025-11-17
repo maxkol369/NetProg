5f3419410b636c0d685195aa975cd3e1520a48c7
daniilalatov <daniil2006alatov@gmail.com>
2025-11-17 14:00:24 +0300

    Добавлен UDP клиент для службы daytime

--- daytime/daytime.cpp
+++ daytime/daytime.cpp
@@ -5,36 +5,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 #include <errno.h>
 
-#define DAYTIME_PORT 13
+#define TARGET_IP "172.16.40.1"
+#define TARGET_PORT 13
 #define BUFFER_SIZE 256
-#define TIMEOUT 10  // Увеличили таймаут
+#define TIMEOUT 10
 
-int main(int argc, char *argv[]) {
+int main() {
     int sockfd;
     struct sockaddr_in server_addr;
-    struct hostent *server;
     char buffer[BUFFER_SIZE];
     int bytes_received;
     
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
-        fprintf(stderr, "Example: %s time.nist.gov\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+    printf("Daytime Client\n");
+    printf("Target: %s:%d\n", TARGET_IP, TARGET_PORT);
     
-    printf("Resolving hostname: %s\n", argv[1]);
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr, "Error: cannot resolve hostname '%s'\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("Host resolved to IP: %s\n", 
-           inet_ntoa(*(struct in_addr*)server->h_addr));
-    
+    // Создаем UDP сокет
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket creation failed");
@@ -51,12 +38,19 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     
+    // Заполняем структуру сервера
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(DAYTIME_PORT);
-    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    server_addr.sin_port = htons(TARGET_PORT);
     
-    printf("Connecting to %s:%d (UDP)...\n", argv[1], DAYTIME_PORT);
+    // Преобразуем IP-адрес
+    if (inet_pton(AF_INET, TARGET_IP, &server_addr.sin_addr) <= 0) {
+        perror("invalid address");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Connecting to %s:%d (UDP)...\n", TARGET_IP, TARGET_PORT);
     
     // Отправляем пустой запрос
     if (sendto(sockfd, "", 0, 0, 
@@ -66,7 +60,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
     
-    printf("Request sent, waiting for response (timeout: %d seconds)...\n", TIMEOUT);
+    printf("Request sent, waiting for response...\n");
     
     // Получаем ответ
     bytes_received = recvfrom(sockfd, buffer, BUFFER_SIZE - 1, 0, NULL, NULL);
@@ -74,10 +68,6 @@ int main(int argc, char *argv[]) {
     if (bytes_received < 0) {
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
             printf("Timeout: server did not respond within %d seconds\n", TIMEOUT);
-            printf("Possible reasons:\n");
-            printf("1. Server does not support UDP daytime protocol\n");
-            printf("2. Server is down or unreachable\n");
-            printf("3. Firewall blocking the connection\n");
         } else {
             perror("recvfrom failed");
         }
